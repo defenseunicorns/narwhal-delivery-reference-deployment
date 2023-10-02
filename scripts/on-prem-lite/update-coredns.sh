f030c27d3c65ba3906fa9b215e0479408719c81a
Andy Roth <roth.andy@gmail.com>
2023-10-02 12:48:53 -0700

    wip

--- scripts/on-prem-lite/update-coredns.sh
+++ scripts/on-prem-lite/update-coredns.sh
@@ -9,5 +9,5 @@ set -o pipefail
 
 keycloak_ingressgateway_ip=$(kubectl get svc keycloak-ingressgateway -n istio-system -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
 existing_node_hosts=$(kubectl get cm coredns -n kube-system -o jsonpath='{.data.NodeHosts}')
-kubectl get cm coredns -n kube-system -o jsonpath='{.data.NodeHosts}' | grep -q "${keycloak_ingressgateway_ip} keycloak.bigbang.dev" || kubectl patch cm coredns -n kube-system --type='json' -p="[{\"op\": \"replace\", \"path\": \"/data/NodeHosts\", \"value\":\"${existing_node_hosts}\n10.0.255.2 keycloak.bigbang.dev\"}]"
+kubectl get cm coredns -n kube-system -o jsonpath='{.data.NodeHosts}' | grep -q "${keycloak_ingressgateway_ip} keycloak.bigbang.dev" || kubectl patch cm coredns -n kube-system --type='json' -p="[{\"op\": \"replace\", \"path\": \"/data/NodeHosts\", \"value\":\"${existing_node_hosts}\n${keycloak_ingressgateway_ip} keycloak.bigbang.dev\"}]"
 kubectl rollout restart deployment coredns -n kube-system
