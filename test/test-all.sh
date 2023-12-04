1db90af7f5ec08589128dd0145f333ee558acac6
Andy Roth <roth.andy@gmail.com>
2023-12-04 15:31:00 -0800

    wip
    Signed-off-by: Andy Roth <roth.andy@gmail.com>

--- test/test-all.sh
+++ test/test-all.sh
@@ -11,5 +11,5 @@ FAILURE=0
 make _test-infra-up _test-platform-up _test-mission-app-up || FAILURE=1
 [[ $FAILURE -eq 0 ]] && sleep 30
 [[ $FAILURE -eq 0 ]] && make _test-mission-app-test || FAILURE=1
-#make _test-infra-down || FAILURE=1
+make _test-infra-down || FAILURE=1
 exit $FAILURE
