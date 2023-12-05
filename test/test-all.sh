0bae7087579d1f94655e6b819172abb5aeec1b9d
Andy Roth <roth.andy@gmail.com>
2023-12-04 17:22:29 -0800

    wip
    Signed-off-by: Andy Roth <roth.andy@gmail.com>

--- test/test-all.sh
+++ test/test-all.sh
@@ -9,7 +9,7 @@ set -o pipefail
 # 4. Tear down
 FAILURE=0
 make _test-infra-up _test-platform-up _test-mission-app-up || FAILURE=1
-[[ $FAILURE -eq 0 ]] && sleep 30
+[[ $FAILURE -eq 0 ]] && echo "waiting for a few seconds for the app to come up" && sleep 60
 [[ $FAILURE -eq 0 ]] && make _test-mission-app-test || FAILURE=1
 make _test-infra-down || FAILURE=1
 exit $FAILURE
