0a7687d643175c9277e847b5e8cbd5457ccdb48a
Luke Griswold <ldgriswold@defenseunicorns.com>
2023-12-13 14:55:19 -0500

    chore: add domain argument parsing to etc/hosts setup

--- test/update-local-etc-hosts.sh
+++ test/update-local-etc-hosts.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 # Script that will update the local /etc/hosts file with the various ingress gateway IP addresses
+# get domain from argument
+DOMAIN=$1
+if [ -z "$DOMAIN" ]; then
+  DOMAIN="bigbang.dev"
+fi
+echo "Setting up /etc/hosts for domain: $DOMAIN"
 
 # enable common error handling options
 set -o errexit
@@ -11,6 +17,6 @@ admin_ingressgateway_ip=10.0.255.1
 tenant_ingressgateway_ip=10.0.255.2
 keycloak_ingressgateway_ip=10.0.255.3
 
-grep -qxF "${admin_ingressgateway_ip} kiali.bigbang.dev grafana.bigbang.dev neuvector.bigbang.dev tracing.bigbang.dev " /etc/hosts || echo "${admin_ingressgateway_ip} kiali.bigbang.dev grafana.bigbang.dev neuvector.bigbang.dev tracing.bigbang.dev" | tee -a /etc/hosts
-grep -qxF "${keycloak_ingressgateway_ip} keycloak.bigbang.dev" /etc/hosts || echo "${keycloak_ingressgateway_ip} keycloak.bigbang.dev" | tee -a /etc/hosts
-grep -qxF "${tenant_ingressgateway_ip} podinfo.bigbang.dev" /etc/hosts || echo "${tenant_ingressgateway_ip} podinfo.bigbang.dev" | tee -a /etc/hosts
+grep -qxF "${admin_ingressgateway_ip} kiali.$DOMAIN grafana.$DOMAIN neuvector.$DOMAIN tracing.$DOMAIN " /etc/hosts || echo "${admin_ingressgateway_ip} kiali.$DOMAIN grafana.$DOMAIN neuvector.$DOMAIN tracing.$DOMAIN" | tee -a /etc/hosts
+grep -qxF "${keycloak_ingressgateway_ip} keycloak.$DOMAIN" /etc/hosts || echo "${keycloak_ingressgateway_ip} keycloak.$DOMAIN" | tee -a /etc/hosts
+grep -qxF "${tenant_ingressgateway_ip} podinfo.$DOMAIN" /etc/hosts || echo "${tenant_ingressgateway_ip} podinfo.$DOMAIN" | tee -a /etc/hosts
